83ad193997358ccc5a54ceab7af132e1e6d354d9
alexciccarese <alex.ciccarese@gmail.com>
2025-04-01 16:31:42 +0200

    Enhance Footer component with new layout and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,44 @@ export default function Footer() {
   return (
 
     <>
-      <footer className="bg-dark">
-        <div className="container">
-          <h1>FOOTER</h1>
+      <footer className="text-light bg-dark pb-4">
+        <div className="container mt-5">
+          <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-5 g-3">
+
+            <div className="col">
+              LOGO
+
+              <p className="mt-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis accusamus voluptate iusto ratione, cupiditate quis, eaque porro error magni dolores tempora facilis, adipisci quasi perferendis perspiciatis. Dolore deleniti quia repudiandae?</p>
+            </div>
+
+            <div className="col"></div>
+
+            <div className="col">
+              LOREM
+
+              <ul class="list-group list-group-flush mt-2">
+                <li class="list-group"><a href="">An item</a></li>
+                <li class="list-group"><a href="">A second item</a></li>
+                <li class="list-group"><a href="">A third item</a></li>
+                <li class="list-group"><a href="">A fourth item</a></li>
+                <li class="list-group"><a href="">And a fifth one</a></li>
+              </ul>
+            </div>
+
+            <div className="col">
+              LOREM
+
+              <ul class="list-group list-group-flush mt-2">
+                <li class="list-group"><a href="">An item</a></li>
+                <li class="list-group"><a href="">A second item</a></li>
+                <li class="list-group"><a href="">A third item</a></li>
+                <li class="list-group"><a href="">A fourth item</a></li>
+                <li class="list-group"><a href="">And a fifth one</a></li>
+              </ul>
+            </div>
+
+
+          </div>
         </div>
       </footer>
     </>
